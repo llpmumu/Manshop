6d07678cfccf2fbd0598627ee0b5ea582a9c7887
llpmumu <824484941@qq.com>
2018-01-02 18:20:55 +0800

    ...

--- app/src/main/java/com/manshop/android/ui/view/PersonalFragment.java
+++ app/src/main/java/com/manshop/android/ui/view/PersonalFragment.java
@@ -4,12 +4,14 @@ package com.manshop.android.ui.view;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.manshop.android.R;
 
@@ -32,6 +34,15 @@ public class PersonalFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_personal, container, false);
+        final TextView imlogin = (TextView) view.findViewById(R.id.imlogin);
+        imlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imlogin.setVisibility(View.INVISIBLE);
+                Intent intent = new Intent(getActivity(),LoginActivity.class);
+                startActivity(intent);
+            }
+        });
         LvPer = (ListView) view.findViewById(R.id.lv_per);
         initPer();
         adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, mPerList);
