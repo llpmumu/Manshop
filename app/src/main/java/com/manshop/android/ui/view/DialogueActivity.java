212743d1ae886a2b4e7479f7f00ec8038ac857b2
llpmumu <824484941@qq.com>
2018-01-03 22:51:56 +0800

    ..

---
+++ app/src/main/java/com/manshop/android/ui/view/DialogueActivity.java
@@ -0,0 +1,15 @@
+package com.manshop.android.ui.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.manshop.android.R;
+
+public class DialogueActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialogue);
+    }
+}
