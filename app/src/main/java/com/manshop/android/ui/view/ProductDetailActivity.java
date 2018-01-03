e78f62205b33a9db2e096f1c898e2bad1e2f5cc4
llpmumu <824484941@qq.com>
2018-01-03 11:57:35 +0800

    ...

--- app/src/main/java/com/manshop/android/ui/view/ProductDetailActivity.java
+++ app/src/main/java/com/manshop/android/ui/view/ProductDetailActivity.java
@@ -10,6 +10,6 @@ public class ProductDetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_product_detail);
+        setContentView(R.layout.activity_good_detail);
     }
 }
