9436d0fcbe2fc44671f9ab3f52234005084cb446
llpmumu <824484941@qq.com>
2018-01-04 10:26:28 +0800

    ...

--- app/src/main/java/com/manshop/android/adapter/MsgAdapter.java
+++ app/src/main/java/com/manshop/android/adapter/MsgAdapter.java
@@ -1,9 +1,65 @@
 package com.manshop.android.adapter;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.manshop.android.R;
+import com.manshop.android.model.Msg;
+
+import java.util.List;
 
 /**
- * Created by Lin on 2018/1/3.
+ * Created by Administrator on 2017/10/19.
  */
 
-public class MsgAdapter {
+public class MsgAdapter extends RecyclerView.Adapter<MsgAdapter.ViewHolder> {
+    private List<Msg> mMsgList;
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        LinearLayout leftLayout;
+        LinearLayout rightLayout;
+        TextView leftMsg;
+        TextView rightMag;
+
+        public ViewHolder(View view) {
+            super(view);
+            leftLayout = (LinearLayout) view.findViewById(R.id.left_layout);
+            rightLayout = (LinearLayout) view.findViewById(R.id.right_layout);
+            leftMsg = (TextView) view.findViewById(R.id.left_msg);
+            rightMag = (TextView) view.findViewById(R.id.right_msg);
+        }
+    }
+
+    public MsgAdapter(List<Msg> msgList) {
+        mMsgList = msgList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_dialogue, parent, false);
+        return new ViewHolder(view);
+    }
+
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Msg msg=mMsgList.get(position);
+        if(msg.getType()==Msg.TYPE_RECEIVED){
+            holder.leftLayout.setVisibility(View.VISIBLE);
+            holder.rightLayout.setVisibility(View.GONE);
+            holder.leftMsg.setText(msg.getContent());
+        }else if (msg.getType()==Msg.TYPE_SENT){
+            holder.rightLayout.setVisibility(View.VISIBLE);
+            holder.leftLayout.setVisibility(View.GONE);
+            holder.rightMag.setText(msg.getContent());
+        }
+    }
+    @Override
+    public int getItemCount() {
+        return mMsgList.size();
+    }
 }
