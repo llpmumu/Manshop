353888ffeb6ee5825eebfde65a7d1737e180712a
llpmumu <824484941@qq.com>
2018-01-12 11:41:06 +0800

    ...

---
+++ app/src/main/java/com/manshop/android/ui/view/WelcomeActivity.java
@@ -0,0 +1,27 @@
+package com.manshop.android.ui.view;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.manshop.android.R;
+import com.manshop.android.ui.base.BaseActivity;
+
+public class WelcomeActivity extends BaseActivity {
+    private final int SPLASH_DISPLAY_LENGHT = 3000;  //延迟3秒
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(intent);
+                WelcomeActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGHT);
+    }
+
+}
