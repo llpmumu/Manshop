d8e3cf91aa7461b2fd5b8d22e2c4f7c67bfc0228
llpmumu <824484941@qq.com>
2018-04-20 14:58:10 +0800

    .........

--- app/src/main/java/com/manshop/android/ui/view/activity/AnimeDetailActivity.java
+++ app/src/main/java/com/manshop/android/ui/view/activity/AnimeDetailActivity.java
@@ -3,6 +3,10 @@ package com.manshop.android.ui.view.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import com.bin.david.form.core.SmartTable;
 import com.bin.david.form.data.Column;
@@ -14,8 +18,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AnimeDetailActivity extends AppCompatActivity {
+    private final int WC = ViewGroup.LayoutParams.WRAP_CONTENT;
+    private final int MP = ViewGroup.LayoutParams.MATCH_PARENT;
+    //    private SmartTable table;
+    private TableLayout table;
+
     private List<RoleAndAkira> list = new ArrayList<>();
-    private SmartTable table;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,17 +34,35 @@ public class AnimeDetailActivity extends AppCompatActivity {
     }
 
     public void init() {
-        table = (SmartTable<RoleAndAkira>) findViewById(R.id.table_akira);
-        list.add(new RoleAndAkira("111","1111"));
-        list.add(new RoleAndAkira("111","1111"));
-        list.add(new RoleAndAkira("111","1111"));
-        list.add(new RoleAndAkira("111","1111"));
-        table.setData(list);
+//        table = (SmartTable<RoleAndAkira>) findViewById(R.id.table_akira);
+//        list.add(new RoleAndAkira("111","1111"));
+//        list.add(new RoleAndAkira("111","1111"));
+//        list.add(new RoleAndAkira("111","1111"));
+//        list.add(new RoleAndAkira("111","1111"));
+//        table.setData(list);
+//
+//        final Column<String> roleColumn = new Column<>("角色", "name");
+//        final Column<String> akireColumn = new Column<>("声优", "age");
+//
+//        final TableData<RoleAndAkira> tableData= new TableData<>("测试",list,roleColumn,akireColumn);
+//        table.setTableData(tableData);
+        table = (TableLayout) findViewById(R.id.table_akira);
+        //清除表格所有行
+        table.removeAllViews();
+        //全部列自动填充空白处
+        table.setStretchAllColumns(true);
+        for (int i = 1; i <= 5; i++) {
+            TableRow tableRow = new TableRow(AnimeDetailActivity.this);
+            for (int j = 1; j <= 2; j++) {
+                //tv用于显示
+                TextView tv = new TextView(AnimeDetailActivity.this);
+                tv.setText("(" + i + "," + j + ")");
 
-        final Column<String> roleColumn = new Column<>("角色", "name");
-        final Column<String> akireColumn = new Column<>("声优", "age");
+                tableRow.addView(tv);
+            }
+            //新建的TableRow添加到TableLayout
+            table.addView(tableRow, new TableLayout.LayoutParams(MP, WC, 1));
 
-        final TableData<RoleAndAkira> tableData= new TableData<>("测试",list,roleColumn,akireColumn);
-        table.setTableData(tableData);
+        }
     }
 }
